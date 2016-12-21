caa4de72a955e26628a173da73b78a72ac1c88c4
zhaojy <victoryai@126.com>
2016-12-21 17:30:52 +0800

    #2016-12-21 使用@ThreadSafe后，不适用原子基本类型的代码测试

--- src/com/std/servlet/CountingFactorizer.java
+++ src/com/std/servlet/CountingFactorizer.java
@@ -8,11 +8,20 @@ import java.io.PrintWriter;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
- * 测试线程安全
+ * 测试线程安全<p>
+ * 注意：<p>
+ * 如果不使用@ThreadSafe注解:<p>
+ * 1.AtomicLong可以保证为线程安全（原子线程安全）的，但仅限该对象<p>
+ * 2.secCnt,线程非安全，会有问题。<p>
+ * 使用@ThreadSafe注解：<p>
+ * 1.AtomicLong可以保证为线程安全<p>
+ * 2.secCnt,线程非安全，会有问题。需要其他线程安全机制保证同步<p>
  */
 @ThreadSafe
 public class CountingFactorizer extends GenericServlet implements Servlet {
     private final AtomicLong count = new AtomicLong(0);  // 原子类型Long
+
+    private Long secCnt = 0L;
 
     public Long getCount() {
         return count.get();
@@ -21,8 +30,9 @@ public class CountingFactorizer extends GenericServlet implements Servlet {
     @Override
     public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
         long curCnt = count.incrementAndGet();
+        secCnt++;
         PrintWriter writer = servletResponse.getWriter();
-        writer.write("current Cnt is : " + curCnt);
+        writer.write("current Cnt is : " + curCnt + ",secCnt is:" + secCnt);
         writer.close();
     }
 }
