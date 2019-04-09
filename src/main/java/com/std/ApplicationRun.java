0af74ea45a4570964c6000c05de201876a0e55dd
zhaojiayang <zhaojiayang@baiwang.com>
2019-04-09 17:12:12 +0800

    #2019-04-09 配置文件提交

--- src/main/java/com/std/ApplicationRun.java
+++ src/main/java/com/std/ApplicationRun.java
@@ -3,9 +3,11 @@ package com.std;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.PropertySource;
 
 @ServletComponentScan(basePackages = "com.std")
 @SpringBootApplication(scanBasePackages = "com.std")
+@PropertySource(value = "classpath:my.properties", encoding = "utf-8")
 public class ApplicationRun {
 
     public static void main(String[] args) {
--- src/main/java/com/std/springboot/filter/CustomerFilter.java
+++ src/main/java/com/std/springboot/filter/CustomerFilter.java
@@ -9,13 +9,22 @@ import java.io.IOException;
 
 /**
  * 自定义过滤器 需要实现Filter
+ * <p>
+ * **** 过滤器的顺序问题：
+ * 使用FilterRegistrationBean实现
  *
  * @author zhaojy
  * @date 2019-03-18
  */
-@WebFilter(urlPatterns = "/user/*")
+@WebFilter(urlPatterns = "/user/*", filterName = "customerFilter")
 @Slf4j
 public class CustomerFilter implements Filter {
+
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        log.info("filter 初始化");
+    }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws
