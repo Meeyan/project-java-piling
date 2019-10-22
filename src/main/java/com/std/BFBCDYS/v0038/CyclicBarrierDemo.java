37e0b4adbc0891f1c71b6e42c2b9260e550d8ec6
meeyan <victoryai@126.com>
2019-10-22 23:32:02 +0800

    1. 完善CyclicBarrier实例 2. 加入CyclicBarrier源码解读

--- src/main/java/com/std/BFBCDYS/v0038/CyclicBarrierDemo.java
+++ src/main/java/com/std/BFBCDYS/v0038/CyclicBarrierDemo.java
@@ -9,10 +9,30 @@ import java.util.concurrent.CyclicBarrier;
  * @date 2019/10/13 14:05
  */
 public class CyclicBarrierDemo {
+
+    public void meeting(CyclicBarrier cyclicBarrier) {
+        System.out.println(Thread.currentThread().getName() + " 到达会议室，准备开会..");
+        try {
+            cyclicBarrier.await();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println(Thread.currentThread().getName() + " 会议中，准备发言..");
+    }
+
     public static void main(String[] args) {
         // 初始化10个
         CyclicBarrier meetingStartE = new CyclicBarrier(10, () -> {
             System.out.println("人到齐了，我们开始开会...");
         });
+
+        // 类似会议室
+        CyclicBarrierDemo meetingRoom = new CyclicBarrierDemo();
+
+        for (int i = 0; i < 10; i++) {
+            new Thread(() -> {
+                meetingRoom.meeting(meetingStartE);
+            }).start();
+        }
     }
 }
