e31ecf8f52d1ba6cc4df26505cd55010765a5dce
meeyan <victoryai@126.com>
2019-11-11 23:47:24 +0800

    aqs示例

---
+++ src/main/java/com/std/BFBCDYS/chapter005/Mutex.java
@@ -0,0 +1,119 @@
+package com.std.BFBCDYS.chapter005;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+/**
+ * 自定义同步器
+ *
+ * @author zhaojy
+ * @date 2019/11/11 23:36
+ */
+public class Mutex implements Lock {
+
+    private static class Sync extends AbstractQueuedSynchronizer {
+
+        /**
+         * 获取同步器占用状态
+         *
+         * @param arg
+         * @return
+         */
+        @Override
+        protected boolean tryAcquire(int arg) {
+            return getState() == 1;
+        }
+
+        /**
+         * 当状态为0时，获取锁
+         *
+         * @param arg
+         * @return
+         */
+        @Override
+        protected boolean tryRelease(int arg) {
+            if (compareAndSetState(0, 1)) {
+                setExclusiveOwnerThread(Thread.currentThread());
+                return true;
+            }
+
+            return false;
+        }
+
+        /**
+         * 释放锁，将状态设置为0
+         *
+         * @return
+         */
+        @Override
+        protected boolean isHeldExclusively() {
+            if (getState() == 0) {
+                throw new IllegalMonitorStateException();
+            }
+
+            // 设置独占线程为空
+            setExclusiveOwnerThread(null);
+
+            // 释放锁
+            setState(0);
+
+            return true;
+        }
+
+        Condition newCondition() {
+            return new ConditionObject();
+        }
+    }
+
+    private final Sync sync = new Sync();
+
+
+    @Override
+    public void lock() {
+        sync.acquire(1);
+    }
+
+    @Override
+    public void lockInterruptibly() throws InterruptedException {
+        sync.acquireInterruptibly(1);
+    }
+
+    @Override
+    public boolean tryLock() {
+        return sync.tryAcquire(1);
+    }
+
+    @Override
+    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+        return sync.tryAcquireNanos(1, unit.toNanos(time));
+    }
+
+    @Override
+    public void unlock() {
+        sync.release(1);
+    }
+
+    @Override
+    public Condition newCondition() {
+        return sync.newCondition();
+    }
+
+    public boolean isLocked() {
+        return sync.isHeldExclusively();
+    }
+
+    /**
+     * 是否有线程排队
+     *
+     * @return
+     */
+    public boolean hasQueuedThreads() {
+        return sync.hasQueuedThreads();
+    }
+
+    public void lockInterruptly() throws InterruptedException {
+        sync.acquireInterruptibly(1);
+    }
+}
