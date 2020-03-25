ea1643138686d68eda0abb6719139bdc5c601765
zhaojy <zhaojiayang@baiwang.com>
2020-03-25 18:29:45 +0800

    demo commit

--- src/main/java/com/std/netty/v063_nettychat/Demo.java
+++ src/main/java/com/std/netty/v063_nettychat/Demo.java
@@ -5,4 +5,12 @@ package com.std.netty.v063_nettychat;
  * @date 2020/3/20 3:48 PM
  */
 public class Demo {
+    public static void main(String[] args) {
+        ClassLoader classLoader = Demo.class.getClassLoader();
+        System.out.println(classLoader);
+        ClassLoader classLoaderParent = classLoader.getParent();
+        System.out.println(classLoaderParent);
+
+
+    }
 }
