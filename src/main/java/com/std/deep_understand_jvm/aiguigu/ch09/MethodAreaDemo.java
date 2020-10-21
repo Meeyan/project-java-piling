fa4fd815ef0dfdacd4adc57378b19529bd233afd
zhaojy <victoryai@126.com>
2020-10-21 22:53:46 +0800

    demo 提交

---
+++ src/main/java/com/std/deep_understand_jvm/aiguigu/ch09/MethodAreaDemo.java
@@ -0,0 +1,13 @@
+package com.std.deep_understand_jvm.aiguigu.ch09;
+
+/**
+ * some desc
+ *
+ * @author zhaojy
+ * @date 2020-09-12 16:49
+ */
+public class MethodAreaDemo {
+    public static void main(String[] args) throws InterruptedException {
+        Thread.sleep(1000000);
+    }
+}
